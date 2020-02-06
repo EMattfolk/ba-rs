fcff1b57b6d9ec3dfb90649ec96ccbb3b413ac42
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-02-06 23:03:09 +0100

    Add disown and comments to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/bash
+
+# Kill current running bar
 killall bardata
 killall lemonbar
+
+# Compile and install
 cargo build --release
 cp target/release/bardata ~/.local/bin/bardata
+
+# Start the bar
 ./togglebar &
+disown %./togglebar
